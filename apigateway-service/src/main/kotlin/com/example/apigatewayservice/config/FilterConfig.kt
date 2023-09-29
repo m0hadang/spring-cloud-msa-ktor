5436310f00d60b582a5dc945b2d881d6bed380df
mohadang <ehdlaos@gmail.com>
2023-09-29 19:49:57 +0900

    apigateway-service 설정 변경
    - application.yaml 파일 설정 대신 코드 비하인드에서 설정
    - FilterConfig Bean 등록 방식
    - *-request, *-response 요청에 대하여 헤더 추가 처리

---
+++ apigateway-service/src/main/kotlin/com/example/apigatewayservice/config/FilterConfig.kt
@@ -0,0 +1,31 @@
+package com.example.apigatewayservice.config
+
+import org.springframework.cloud.gateway.route.RouteLocator
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class FilterConfig {
+    @Bean
+    fun gatewayRoutes(builder: RouteLocatorBuilder): RouteLocator {
+        return builder.routes()
+            .route { r ->
+                r.path("/first-service/**").filters { filter ->
+                    filter
+                        .addRequestHeader("first-request", "first-request-header")
+                        .addResponseHeader("first-response", "first-response-header")
+                }
+                    .uri("http://localhost:8081")
+            }
+            .route { r ->
+                r.path("/second-service/**").filters { filter ->
+                    filter
+                        .addRequestHeader("second-request", "second-request-header")
+                        .addResponseHeader("second-response", "second-response-header")
+                }
+                    .uri("http://localhost:8082")
+            }
+            .build()
+    }
+}
