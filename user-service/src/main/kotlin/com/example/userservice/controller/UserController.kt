bbd24d7de87518361980dde785f65f6f287bbb68
mohadang <ehdlaos@gmail.com>
2023-09-29 21:02:12 +0900

    Environment 사용하여 application.yaml의 메시지 사용

--- user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
@@ -1,14 +1,21 @@
 package com.example.userservice.controller
 
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/")
-class UserController {
+class UserController(
+    val env: Environment,
+) {
     @GetMapping("/health_check")
-    fun status() : String {
+    fun HealthCheck() : String {
         return "It's working in user service"
+    }
+    @GetMapping("/welcome")
+    fun Welcome() : String {
+        return env.getProperty("greeting.message").toString()
     }
 }
