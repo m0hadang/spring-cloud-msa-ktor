0736ad2b86a9a5af61439a42bf00ff61477ef976
mohadang <ehdlaos@gmail.com>
2023-09-29 19:59:45 +0900

    first-service-tester, second-service-tester message API 추가
    - apigateway-service에 의해 message api 라우팅 정상 처리.

--- first-service-tester/src/main/kotlin/com/example/firstservicetester/FirstServiceController.kt
+++ first-service-tester/src/main/kotlin/com/example/firstservicetester/FirstServiceController.kt
@@ -1,14 +1,24 @@
 package com.example.firstservicetester
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/first-service")
 class FirstServiceController {
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
+
     @GetMapping("/welcome")
-    fun hello() : String {
+    fun hello(): String {
         return "Welcome to the First service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("first-request") header: String): String {
+        log.info(header)
+        return "This is a message from the First service."
     }
 }
--- second-service-tester/src/main/kotlin/com/example/secondservicetester/SecondServiceController.kt
+++ second-service-tester/src/main/kotlin/com/example/secondservicetester/SecondServiceController.kt
@@ -1,14 +1,24 @@
 package com.example.secondservicetester
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/second-service")
 class SecondServiceController {
+    private val log = LoggerFactory.getLogger(this.javaClass)!!
+
     @GetMapping("/welcome")
-    fun hello() : String {
+    fun hello(): String {
         return "Welcome to the Second service."
+    }
+
+    @GetMapping("/message")
+    fun message(@RequestHeader("second-request") header: String): String {
+        log.info(header)
+        return "This is a message from the Second service."
     }
 }
