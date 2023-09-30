5ab0dc5160ca04897d076c9e699338340d25e348
mohadang <ehdlaos@gmail.com>
2023-09-30 10:37:36 +0900

    user-service h2database(in memory DB) 추가

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("com.h2database:h2:2.2.224")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
