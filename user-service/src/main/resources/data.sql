f3fba246d57ac0e7db87373a73e1195d0fc49d11
mohadang <ehdlaos@gmail.com>
2023-09-30 16:17:54 +0900

    user-service 기본 데이터 추가

---
+++ user-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users(name, email, user_id, encrypted_pwd)
+    values ('user1', 'test123@test.com', '1111', '12345');
+insert into users(name, email, user_id, encrypted_pwd)
+    values ('user2', 'test2@test.com', '2222', 'a!@#ASD');
+insert into users(name, email, user_id, encrypted_pwd)
+    values ('user3', 'test3@test.com', '3333', 'ASDZXCw');
