6fa52fff7ab6d6ec6ff41c11dffb2662ad394996
mohadang <ehdlaos@gmail.com>
2023-09-30 15:07:16 +0900

    catalog-service 유레카 등록 안되는 문제 수정

--- catalog/build.gradle.kts
+++ catalog/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     implementation("org.modelmapper:modelmapper:3.1.1")
