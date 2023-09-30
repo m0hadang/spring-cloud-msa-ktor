ea9787829620d943aa2a10cb382659fce19b6379
mohadang <ehdlaos@gmail.com>
2023-09-30 14:50:49 +0900

    catalog service 실행시 데이터 자동 추가 설정

---
+++ catalog/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into catalogs(product_id, product_name, stock, unit_price)
+    values ('CATALOG-001', 'Berlin', 100, 1500);
+insert into catalogs(product_id, product_name, stock, unit_price)
+    values ('CATALOG-002', 'Tokyo', 110, 2000);
+insert into catalogs(product_id, product_name, stock, unit_price)
+    values ('CATALOG-003', 'Stockholm', 120, 5000);
+insert into catalogs(product_id, product_name, stock, unit_price)
+    values ('CATALOG-004', 'Berlin', 130, 3000);
