b61caa1ebff9c5fef6cdbafc245a7e26df73d1a2
mohadang <ehdlaos@gmail.com>
2023-09-30 12:31:20 +0900

    UserEntity(entity, data class) 추가
    - JPA로 데이터를 사용하기 위한 객체
    - UserDto를 UserEntity로 변환하여 JPA API 사용 가능

---
+++ user-service/src/main/kotlin/com/example/userservice/jpa/UserEntity.kt
@@ -0,0 +1,20 @@
+package com.example.userservice.jpa
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "users")
+data class UserEntity(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(nullable = false, length = 50, unique = true)
+    var email: String = "",
+    @Column(nullable = false, length = 50)
+    var name: String = "",
+    @Column(nullable = false, unique = true)
+    var userId: String = "",
+    @Column(nullable = false, unique = true)
+    var encryptedPwd: String = "",
+)
