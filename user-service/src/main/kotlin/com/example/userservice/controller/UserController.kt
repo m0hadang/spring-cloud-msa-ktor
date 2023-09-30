3ccc583e950d32090b31a279a6b812fdf4555493
mohadang <ehdlaos@gmail.com>
2023-09-30 12:36:39 +0900

    User 추가 기능 구현

--- user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/example/userservice/controller/UserController.kt
@@ -1,24 +1,39 @@
 package com.example.userservice.controller
 
+import com.example.userservice.dto.UserDto
+import com.example.userservice.service.UserService
 import com.example.userservice.vo.Greeting
+import com.example.userservice.vo.RequestUser
+import org.modelmapper.ModelMapper
+import org.modelmapper.convention.MatchingStrategies
 import org.springframework.core.env.Environment
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
 class UserController(
     val env: Environment,
-    val greeting: Greeting,
+    private val greeting: Greeting,
+    private val userService: UserService,
 ) {
     @GetMapping("/health_check")
-    fun HealthCheck() : String {
+    fun HealthCheck(): String {
         return "It's working in user service"
     }
+
     @GetMapping("/welcome")
-    fun Welcome() : String {
+    fun Welcome(): String {
 //        return env.getProperty("greeting.message").toString()
         return greeting.message
+    }
+
+    @PostMapping("/user")
+    fun createUser(@RequestBody user: RequestUser): String {
+        val mapper = ModelMapper();
+        mapper.configuration.setMatchingStrategy(MatchingStrategies.STRICT)
+
+        val userDto = mapper.map(user, UserDto::class.java)
+        userService.createUser(userDto)
+        return "Create user method is called"
     }
 }
